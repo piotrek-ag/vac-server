eccc9bf66c1106d0ec5a8a79ca181e7b20777e0d
piotrek-ag <piotr.agier@gmail.com>
2023-08-07 20:14:32 +0200

    Cascade delete related sounds when removing persona

--- resources/personas.py
+++ resources/personas.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 
 from services.personas_s import get_personas, save_personas
-from services.sounds_s import generate_lines_and_sounds
+from services.sounds_s import generate_lines_and_sounds, get_sounds, save_sounds
 from utils.util import generate_id
 
 app = Flask(__name__)
@@ -52,9 +52,27 @@ class Persona(Resource):
         return persona
 
     def delete(self, id):
-        personas = [p for p in get_personas() if p['id'] != id]
+        # Retrieve all personas
+        personas = get_personas()
+
+        # Get the persona with the specified id
+        persona = next((p for p in personas if p['id'] == id), None)
+
+        # Delete the sounds associated with this persona
+        if persona:
+            sounds_to_delete = persona['sounds']
+            sounds = get_sounds()
+            # Filter out the sounds that need to be deleted
+            sounds = [s for s in sounds if s['id'] not in sounds_to_delete]
+            save_sounds(sounds)
+
+        # Remove the persona from the personas list
+        personas = [p for p in personas if p['id'] != id]
+
+        # Save the updated personas list
         save_personas(personas)
-        return {'result': 'Persona deleted'}
+
+        return {'result': 'Persona and associated sounds deleted'}
 
 
 class AllPersonas(Resource):
